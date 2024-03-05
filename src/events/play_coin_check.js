973ff56e95c8d8001fe65a302d45c8917a120da9
The_UltimateNuke <84410206+TheUltimateNuke@users.noreply.github.com>
2024-03-05 13:29:36 +0000

    moving

--- src/events/play_coin_check.js
+++ src/events/play_coin_check.js
@@ -8,13 +8,8 @@ module.exports = {
   name: Events.MessageCreate,
   async execute(interaction) {
     if (interaction.content.toLowerCase().match(/\b(c\!playcoins)\b/) != null && !interaction.author.bot) {
-      const rand = Math.floor(randomNumber(0, 39))
-      if (rand == 0) {
-        await interaction.reply("You have " + rand + " PlayCoins... (THERES NOTHING HERE yet, wait for HL2VRAI)") //TODO: change for HL2VRAI
-      }
-      else {
-        await interaction.reply("You have " + rand + " PlayCoins!")
-      }
+      const rand = Math.floor(randomNumber(1, 39))
+      await interaction.reply("You have " + rand + " PlayCoins! Don't hit zero, Gordon!")
     }
   },
 };
