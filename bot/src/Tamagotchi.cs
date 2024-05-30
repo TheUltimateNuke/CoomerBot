834b7ef8187cec1f1f5224db1f0b17ac1c3b29b6
The_UltimateNuke _ <nuke.public@gmail.com>
2024-05-30 16:35:04 +0000

    more work done on Tamagotchi.cs

--- bot/src/Tamagotchi.cs
+++ bot/src/Tamagotchi.cs
@@ -1,22 +1,50 @@
 using Discord.Interactions;
+using Discord.Rest;
 
 namespace CoomerBot;
 
 public class Tamagotchi : InteractionModuleBase
 {
-    public struct TamagotchiNeeds 
+    public class Need
     {
-        public float hunger = 100f;
-        public float thirst = 100f;
+        public string name = "Untitled Need";
+        public float maxValue = 100;
+        public float decayRate = 0.03f;
+        public float curValue;
 
-        public TamagotchiNeeds() { }
+        public Need() { curValue = maxValue; }
     }
 
     private const string commandFailureMessage = "...Hello, Gordon!";
 
-    public bool TamagotchiActive {get; private set;}
+    public static TimeSpan? StartActiveHours { get; private set; }
+    public static TimeSpan? EndActiveHours { get; private set; }
 
-    public readonly TamagotchiNeeds needs = new();
+    public bool TamagotchiActive 
+    {
+        get
+        {
+            return TamagotchiActive; 
+        } 
+        private set
+        {
+            SetTamagotchiMode(value);
+        }
+    }
+
+    public Need[] Needs => needs.ToArray();
+    private readonly List<Need> needs = new() 
+    {
+        new Need()
+        {
+            name = "Hunger"
+        },
+        new Need()
+        {
+            name = "Thirst",
+            decayRate = 0.06f
+        }
+    };
 
     private PeriodicTimer? tickTimer;
 
@@ -26,7 +54,7 @@ public class Tamagotchi : InteractionModuleBase
         switch (input.ToLower()) 
         {
             case "toggle":
-                await SetTamagotchiMode(!TamagotchiActive);
+                TamagotchiActive = !TamagotchiActive;
                 break;
             default:
                 await RespondAsync($"{commandFailureMessage}\n\n||This message is being displayed because the passed command went unrecognized.||");
@@ -34,22 +62,60 @@ public class Tamagotchi : InteractionModuleBase
         }
     }
 
+    [SlashCommand("activehours", "Sets a time range where CoomerBot is \"awake\" and his needs are depleting.")]
+    public async Task SetActiveHours(string input) 
+    {
+        var args = input.Split(' ');
+        if (args.Length != 2) 
+        {
+            await RespondAsync($"{commandFailureMessage}\n\n||This message is being displayed because the incorrect amount of arguments for this command was passed.||");
+            return;
+        }
+
+        if (!TimeSpan.TryParse(args[0], out var startTime)) 
+        {
+            await RespondAsync($"{commandFailureMessage}\n\n||This message is being displayed because CoomerBot could not parse \"{args[0]}\" as a time of day. Please input a correctly formatted 24-hour clock time.||");
+            return;
+        }
+
+        if (!TimeSpan.TryParse(args[1], out var endTime)) 
+        {
+            await RespondAsync($"{commandFailureMessage}\n\n||This message is being displayed because CoomerBot could not parse \"{args[1]}\" as a time of day. Please input a correctly formatted 24-hour clock time.||");
+            return;
+        }
+
+        StartActiveHours = startTime;
+        EndActiveHours = endTime;
+    }
+
     private async Task StartTamagotchiTick() 
     {
         tickTimer ??= new(TimeSpan.FromSeconds(10));
-
         while (await tickTimer.WaitForNextTickAsync()) MainTickLoop();
     }
 
     private void MainTickLoop()
     {
-        
+        if (!TamagotchiActive) return;
+
+        if (StartActiveHours is not null || EndActiveHours is not null)
+        {
+            var now = DateTime.Now.TimeOfDay;
+
+            if ((now > StartActiveHours) && (now < EndActiveHours))
+            {
+                foreach (Need need in Needs)
+                {
+                    need.curValue -= need.decayRate;
+                }
+            }
+        }   
     }
 
-    public async Task SetTamagotchiMode(bool enabled) 
+    public void SetTamagotchiMode(bool enabled) 
     {
         TamagotchiActive = enabled;
-        if (enabled) await StartTamagotchiTick(); else tickTimer?.Dispose();
+        if (enabled) _ = StartTamagotchiTick(); else tickTimer?.Dispose();
 
         return;
     }
