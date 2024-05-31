91112233146e6e1195c4a25bfc13b5600b071de8
The_UltimateNuke _ <nuke.public@gmail.com>
2024-05-31 16:59:46 +0000

    some organization/workflow changes

---
+++ scripts/publish/update_from_bin.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+mv ./bin/CoomerBot ./CoomerBot
+../../CoomerBot || exit
--- scripts/src/gitpod_publish.sh
+++ scripts/src/gitpod_publish.sh
@@ -3,7 +3,7 @@
 if [ -d /home/gitpod/dotnet/sdk/6.0.423 ] ; then 
     cd /workspace/CoomerBot/ || exit 1
     dotnet publish --output ./Staging -c Release
-    cp -rf ./scripts/publish/ ./Staging/scripts/
+    cp -r ./scripts/publish/ ./Staging/scripts/publish/
 else
     bash ./scripts/src/gitpod_setup.sh
 fi
