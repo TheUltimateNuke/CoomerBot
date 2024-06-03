b6b979ed08ba50d70fdc8e5fd18c642dc95278fd
The_UltimateNuke _ <nuke.public@gmail.com>
2024-06-03 16:17:40 +0000

    auto-update work

--- scripts/src/gitpod_publish.sh
+++ scripts/src/gitpod_publish.sh
@@ -3,7 +3,7 @@
 if [ -d /home/gitpod/dotnet/sdk/6.0.423 ] ; then 
     cd /workspace/CoomerBot/ || exit 1
     dotnet publish --output ./Staging -c Release
-    cp -r ./scripts/publish/ ./Staging/scripts/publish/
+    cp -rf ./scripts/publish/ ./Staging/scripts/
 else
     bash ./scripts/src/gitpod_setup.sh
 fi
