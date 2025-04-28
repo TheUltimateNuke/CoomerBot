e4e918ffdf91347d1fadf163cc090859d86a55e8
TheUltimateNuke <nuke.public@gmail.com>
2025-04-28 17:43:40 +0000

    remove duplicate gitpod scripts

--- scripts/gitpod_publish.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -d /home/gitpod/dotnet/sdk/6.0.423 ] ; then 
-    cd /workspace/CoomerBot/bot || exit 0
-    dotnet publish --output ../out -c Release
-else
-    bash ./gitpod_setup.sh
-fi
-
